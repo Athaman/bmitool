dc4c10a7d07b70098fc400eb009192e6b1002909
Keone Martin <keonemartin@gmail.com>
2018-02-12 01:18:53 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './App.css';
+import Range from './Components/Range'
+import Output from './Components/Output'
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      height: 170,
+      weight: 65,
+      bmi: 22.49,
+      bmiClass: 'Normal'
+    }
+  }
+
+  heightChange(height){
+    this.setState({height: height}, function(){
+      console.log(this.state);
+    });
+  }
+
+  weightChange(weight){
+    this.setState({weight: weight}, function(){
+      console.log(this.state);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>BMI Calculator</h1>
+        <form>
+          <div>
+            <label>Height</label>
+            <Range value={this.state.height} onChange={this.heightChange.bind(this)}/>
+          </div>
+          <div>
+            <label>Weight</label>
+            <Range value={this.state.weight} onChange={this.weightChange.bind(this)}/>
+          </div>
+        </form>
+        <br /><br />
+        <Output />
+      </div>
+    );
+  }
+}
+
+export default App;
