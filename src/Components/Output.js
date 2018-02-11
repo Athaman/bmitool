ac019432bfe7f3db0f2acee11f66646fe3c13378
Keone Martin <keonemartin@gmail.com>
2018-02-12 01:36:36 +1100

    added in some formulae in case i want to change to pounds/feet

--- src/Components/Output.js
+++ src/Components/Output.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 class Output extends Component {
+  toFeet(cm){
+    let realFeet = ((cm * .3937) / 12);
+    let feet = Math.floor(realFeet);
+    let inches = Math.round((realFeet - feet) * 12);
+    return feet + "'" + inches;
+  }
+
+  toLbs(kg){
+    let nearExact = kg/0.45359237;
+    let lbs = Math.floor(nearExact);
+    return lbs;
+  }
   render() {
+    let height = this.props.data.height;
+    let weight = this.props.data.weight;
+    let bmi = this.props.data.bmi;
+    let bmiClass = this.props.data.bmiClass;
+
     return (
       <div className="output">
-        
+        <h3>{height}</h3>
+        <h3>{weight}</h3>
+        <h3>{bmi}</h3>
+        <h3 className={(this.props.data.bmiClass === "Obese") ? "danger" : ""}>{bmiClass}{(this.props.data.bmiClass === "Obese") ? <a href='http://somedietsite.com'> What Can I Do?</a> : ""} </h3>
       </div>
     );
   }
